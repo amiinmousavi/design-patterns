1432759104e5a6d3b0a65a6265aa364681f75be6
unknown <seyed.mousavi03@gmail.com>
2024-02-04 20:22:25 +0100

    singleton-class example from slides

---
+++ 03-singleton-pattern/05-oplossingen/src/SingletonClass.java
@@ -0,0 +1,11 @@
+public class SingletonClass {
+    private static SingletonClass instance = new SingletonClass();
+
+    private SingletonClass() {
+
+    }
+
+    public static SingletonClass getInstance() {
+        return instance;
+    }
+}
