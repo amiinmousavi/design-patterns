5725f1c9123b1b6c8d5de2646805a32b3c52a84e
unknown <seyed.mousavi03@gmail.com>
2024-02-06 22:31:38 +0100

    command-pattern-2

--- patterns-mosh-hamedani/005-command-pattern/src/code/Main.java
+++ patterns-mosh-hamedani/005-command-pattern/src/code/Main.java
@@ -1,12 +1,24 @@
 package code;
 
-import code.fx.Button;
+//import code.fx.Button;
+
+import code.fx.BlackAndWhiteCommand;
+import code.fx.CompositeCommand;
+import code.fx.ResizeCommand;
 
 public class Main {
     public static void main(String[] args) {
-        var service = new CustomerService();
-        var command = new AddCustomerCommand(service);
-        var button = new Button(command);
-        button.click();
+        //        var service = new CustomerService();
+        //        var command = new AddCustomerCommand(service);
+        //        var button = new Button(command);
+        //        button.click();
+        
+        var composite = new CompositeCommand();
+
+        composite.add(new ResizeCommand());
+        composite.add(new BlackAndWhiteCommand());
+
+        composite.execute();
+        composite.execute();
     }
 }
