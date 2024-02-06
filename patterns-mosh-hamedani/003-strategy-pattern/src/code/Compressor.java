10c51491d51d725e246d1bb52962e2e7e787f46f
unknown <seyed.mousavi03@gmail.com>
2024-02-06 21:01:22 +0100

    strategy pattern

---
+++ patterns-mosh-hamedani/003-strategy-pattern/src/code/Compressor.java
@@ -0,0 +1,5 @@
+package code;
+
+public interface Compressor {
+    void compress(String fileName);
+}
---
+++ patterns-mosh-hamedani/003-strategy-pattern/src/code/Main.java
@@ -0,0 +1,9 @@
+package code;
+
+public class Main {
+    public static void main(String[] args) {
+        var imageStorage = new ImageStorage();
+        imageStorage.store("a", new JpegCompressor(), new BlackAndWhiteFilter());
+        imageStorage.store("a", new PngCompressor(), new BlackAndWhiteFilter());
+    }
+}
