70ad2b1d7a90524b68a786810bf6c7cde623e515
unknown <seyed.mousavi03@gmail.com>
2024-02-09 00:58:08 +0100

    starter observer-pattern

---
+++ patterns-mosh-hamedani/006-observor-pattern/src/code/DataSource.java
@@ -0,0 +1,13 @@
+package code;
+
+public class DataSource {
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    private int value;
+}
